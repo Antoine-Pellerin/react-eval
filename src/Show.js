10ed8f9f0c0a4174842553b6b50e379318d959ec
Antoine PELLERIN <pellerin.antoine@gmail.com>
2021-06-27 17:37:46 +0200

    amélioration des visuels et ajout d'informations

--- src/Show.js
+++ src/Show.js
@@ -1,14 +1,20 @@
+import Fav from "./Fav"
+
 export default function Show(props) {
     const {show} = props
 
     return (
-        <div>
-            <div class="cards">
-                <div class="card-body">
-                    <h5 class="card-title">{show.title}</h5>
-                    <p class="card-text">{show.description}</p>
-                </div>
+        <div class="cards border border-danger m-2 col-2 shadow rounded text-light" >
+            <div class="card-body d-flex flex-column">
+                <img class="card-img-top img-fluid" alt={show.title} src={show.images.poster} />
+                <h5 class="mt-3 card-title">{show.title}<br/><i>({show.creation})</i></h5>
+                <p><b>{show.seasons} seasons</b></p>
+                <p class="card-text text-justify mt-3" style={{height: "15rem", overflow: "scroll", textAlign: "justify"}}>{show.description}</p>
             </div>
+            <div class="d-flex flex-row justify-content-center mt-3 mb-3">
+                <a class="btn btn-outline-danger " href={show.resource_url} role="button" style={{marginTop : "auto"}}>Get more info</a>
+            </div>
+            <Fav />
         </div>
     )
 }
--- src/ShowList.js
+++ src/ShowList.js
@@ -1,8 +1,17 @@
 import React from "react";
 import Show from "./Show";
+import netfli from "./netfli.png"
+
 
 export default function ShowList(props) {
     const elements = props.shows.map(show => <Show key={show.id} show={show} />)
 
-    return (<div>{elements}</div>)
+    return (
+        <div class="bg-dark bg-gradient d-flex flex-wrap justify-content-center">
+            <img src={netfli} alt="Netfli" class="img-fluid mt-5 mb-5" style={{width:"300px"}}/>
+            <div class="d-flex flex-wrap justify-content-center">
+                {elements}
+            </div>
+        </div>
+        )
 }
